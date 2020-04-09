5bfb165f60bf8dd13ce4903aef83571e21323662
dheeraj reddy <anumuladheeraj63@gmail.com>
2020-04-09 17:03:08 +0530

    deleted code from walk-directory to create a pull request

--- walk-directory/main.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const isDirectory=require('./src/validate-path');
-const walkDirectory= require('./src/walk-directory')
-
-const path = process.argv[2];
-
-if(isDirectory(path)){
-    walkDirectory(path).catch(console.error);
-}
